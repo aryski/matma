3d94b16cafa2e85be76f369cb8bb94ebbfeece57
aryski <zakupyadama271@gmail.com>
2023-10-27 13:03:20 +0200

    arrow insertor refact

---
+++ lib/steps_game/bloc/bloc_ext/opposite_arrow_insertor.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:matma/equation/bloc/equation_bloc.dart';
+import 'package:matma/steps_game/bloc/bloc_ext/items_generator.dart';
+import 'package:matma/steps_game/bloc/steps_game_bloc.dart';
+import 'package:matma/steps_game/items/arrow/cubit/arrow_cubit.dart';
+import 'package:matma/steps_game/items/arrow/cubit/arrow_state.dart';
+import 'package:matma/common/items/game_item/cubit/game_item_cubit.dart';
+import 'package:matma/steps_game/items/floor/%20cubit/floor_cubit.dart';
+
+extension OppositeArrowInsertor on StepsGameBloc {
+  Future<bool> handleOppositeInsertion(StepsGameState state,
+      GameItemCubit? item, GameSize gs, Emitter<StepsGameState> emit) async {
+    if (item is! FloorCubit || !(state.isLastItem(item))) return false;
+    var step = state.getStep(item);
+    if (step == null) return false;
+    var dir = step.arrow.state.direction;
+    ArrowCubit arrow;
+    FloorCubit floor;
+    bool isUp = (dir == Direction.up);
+    board.add(EquationEventAddNumber(value: isUp ? -1 : 1));
+    arrow = generateArrow(
+        animationProgress: 0,
+        position:
+            item.state.position + Offset(gs.wUnit * 0.5, isUp ? gs.floorH : 0),
+        size: Offset(gs.wUnit, 0),
+        direction: isUp ? Direction.down : Direction.up);
+    floor = generateFloor(
+        direction: isUp ? Direction.down : Direction.up,
+        position: item.state.position + Offset(gs.wUnit, 0),
+        size: Offset(0, gs.floorH));
+    state.numbers.add(StepsGameNumberState(
+        [StepsGameDefaultItem(arrow: arrow, floor: floor)]));
+    taskCubit.insertedOpposite();
+    item.setLast();
+    emit(state.copy());
+    await Future.delayed(const Duration(milliseconds: 20));
+    if (dir == Direction.up) {
+      floor.updatePosition(Offset(0, gs.hUnit));
+    } else {
+      arrow.updatePosition(Offset(0, -gs.hUnit));
+      floor.updatePosition(Offset(0, -gs.hUnit));
+    }
+
+    floor.updateSizeDelayed(
+        const Duration(milliseconds: 200), Offset(1.25 * gs.wUnit, 0));
+    arrow.animate(1.0);
+    arrow.updateHeight(gs.hUnit);
+
+    floor.setLastLast();
+    item.setNotLastLast();
+    return true;
+  }
+}
